e3d4c28380d623ecd5d3e56d699e7579e309ef42
XavyxS <francisco.sandoval23b@udgvirtual.udg.mx>
2024-12-23 08:56:27 -0600

    Commit 4

--- config/db.js
+++ config/db.js
@@ -1,25 +1,18 @@
 const Sequelize = require("sequelize");
 require('dotenv').config({ path: '.env' });
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    define: {
-      timestamps: true
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    logging: false
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  define: {
+    timestamps: true
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: false
+});
 
-
+module.exports = sequelize;
